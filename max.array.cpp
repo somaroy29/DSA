73234331c7ce059d576332c6fd8d6fa40e8bcbd5
Soma Roy <somaroy21295@gmail.com>
2021-03-01 16:44:04 +0530

    max-array-in-cpp

--- max.array.cpp
+++ max.array.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 void max(int a[],int i,int n){
     int start=a[0];
-    for(i=1;i<n;i++){
+    for(i=0;i<n;i++){
         if(start<a[i]){
             start=a[i];
         }
@@ -10,10 +10,7 @@ void max(int a[],int i,int n){
     cout<<start<<endl;
 }
 int main(){
-    int a[]={5,2,9,6,7};int i;int n=5;
+    int a[]={1,2,8,6,7};int i;int n=5;
     max(a,i,n);
-    // for(i=0;i<=n;i++){
-    //     cout<<a[i]<<endl;
-    // }
  return 0;
 }
